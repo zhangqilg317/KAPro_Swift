4dd814d640d586c47726296777831b095c85ee13
zhangqilg317 <by_zhangqi@163.com>
2016-11-21 11:36:36 +0800

    新建项目

---
+++ KAProSwift/KAProSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KAProSwift
+//
+//  Created by 张奇 on 16/11/21.
+//  Copyright © 2016年 eBest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ KAProSwift/KAProSwiftTests/KAProSwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  KAProSwiftTests.swift
+//  KAProSwiftTests
+//
+//  Created by 张奇 on 16/11/21.
+//  Copyright © 2016年 eBest. All rights reserved.
+//
+
+import XCTest
+@testable import KAProSwift
+
+class KAProSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
