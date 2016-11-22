a5f578753747eb76285570ff385ff9244784c74b
zhangqilg317 <by_zhangqi@163.com>
2016-11-22 16:13:45 +0800

    增加文件的分类，添加网络库

--- KAProSwift/KAProSwift/Other/AppDelegate.swift
+++ KAProSwift/KAProSwift/Other/AppDelegate.swift
@@ -40,7 +40,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    //MARK:
+    //MARK: test
+    func myFirtTestFunc() -> String {
+        let myName = "111"
+        return myName;
+    }
 }
 
